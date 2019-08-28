04caea6e9b8a9d8b9eef65dc164ae2c44a5c1eb9
Anastasia Pakkar <shivasmile11@gmail.com>
2019-08-29 01:30:42 +0300

    Updates

---
+++ precache-manifest.9f4cccd2c7f48a1e0daf7559f96d7178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6b12f422ac0fea113a62660e006787",
+    "url": "/webim-app-test/index.html"
+  },
+  {
+    "revision": "30103ee9be34eb7f1d0c",
+    "url": "/webim-app-test/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "fa7882374dd04cbcb7b2",
+    "url": "/webim-app-test/static/css/main.043dd4fe.chunk.css"
+  },
+  {
+    "revision": "30103ee9be34eb7f1d0c",
+    "url": "/webim-app-test/static/js/2.3e273aab.chunk.js"
+  },
+  {
+    "revision": "fa7882374dd04cbcb7b2",
+    "url": "/webim-app-test/static/js/main.591d86ce.chunk.js"
+  },
+  {
+    "revision": "243a04d23eff47bddda5",
+    "url": "/webim-app-test/static/js/runtime~main.0f21bd65.js"
+  },
+  {
+    "revision": "1c5965c2b1dcdea439b54c3ac60cee38",
+    "url": "/webim-app-test/static/media/ubuntu.1c5965c2.ttf"
+  }
+]);
